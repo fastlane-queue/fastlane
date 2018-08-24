9931125b37d786b4ffd76642ba0d9d1e2a686b2d
Bernardo Heynemann <heynemann@gmail.com>
2018-08-24 16:29:28 -0300

    Job creation

--- easyq/worker/job.py
+++ easyq/worker/job.py
@@ -1,6 +1,7 @@
 from flask import current_app
 from rq import get_current_job
 
+from easyq.models.job import Job
 from easyq.models.task import Task
 
 
@@ -8,34 +9,50 @@ def add_task(task_id):
     task = Task.get_by_task_id(task_id)
 
     job_id = get_current_job().id
-    j = task.create_job(job_id)
+    task.create_job(job_id)
+
+    current_app.job_queue.enqueue(run_job, task_id, job_id, timeout=-1)
 
     return True
 
-    # tag = 'latest'
-    # worker_job = get_current_job(current_app.redis)
 
-    # if worker_job is None:
-    # return False
+def run_job(task_id, job_id):
+    task = Task.get_by_task_id(task_id)
+    job = task.get_job_by_job_id(job_id)
+    logger = current_app.logger.bind(task_id=task_id, job_id=job_id)
 
-    # job = Job.get_by_job_id(worker_job.id)
+    if job is None:
+        return False
 
-    # if job is None:
-    # return False
+    image = job.image
+    tag = 'latest'
 
-    # if ':' in container:
-    # container, tag = container.split(':')
+    if ':' in image:
+        image, tag = image.split(':')
 
-    # job.status = Job.Status.pulling
-    # job.save()
-    # print(f'Downloading updated container image ({container}:{tag})...')
-    # current_app.executor.pull(container, tag)
+    logger = logger.bind(image=image, tag=tag)
 
-    # print(f'Running {command} in {container}:{tag}...')
-    # container_id = current_app.executor.run(container, tag, command)
+    logger.debug('Changing job status...', status=Job.Status.pulling)
+    job.status = Job.Status.pulling
+    task.save()
+    logger.debug('Job status changed successfully.', status=Job.Status.pulling)
 
-    # job.container_id = container_id
-    # job.status = Job.Status.running
-    # job.save()
+    logger.info('Downloading updated container image...', image=image, tag=tag)
+    current_app.executor.pull(image, tag)
+    logger.info('Image downloaded successfully.', image=image, tag=tag)
 
-    # return True
+    logger.info('Running command in container...')
+    container_id = current_app.executor.run(image, tag, job.command)
+    logger.info(
+        'Container started successfully.',
+        image=image,
+        tag=tag,
+        container_id=container_id)
+
+    logger.debug('Changing job status...', status=Job.Status.running)
+    job.container_id = container_id
+    job.status = Job.Status.running
+    task.save()
+    logger.debug('Job status changed successfully.', status=Job.Status.running)
+
+    return True
--- tests/worker/test_job.py
+++ tests/worker/test_job.py
@@ -11,6 +11,9 @@ from easyq.models.task import Task
 def test_run_job(client):
     '''Test run job does not fail if no job available'''
     with client.application.app_context():
+        app = client.application
+        app.redis.flushall()
+
         task_id = str(uuid4())
         t = Task.create_task(task_id, 'container', 'command')
         exec_mock = MagicMock()
@@ -26,3 +29,42 @@ def test_run_job(client):
         expect(t.jobs).to_length(1)
         expect(t.jobs[0].image).to_equal(t.image)
         expect(t.jobs[0].command).to_equal(t.command)
+
+        hash_key = f'rq:job:{t.jobs[0].job_id}'
+
+        res = app.redis.exists(hash_key)
+        expect(res).to_be_true()
+
+        res = app.redis.hget(hash_key, 'status')
+        expect(res).to_equal('finished')
+
+        res = app.redis.hexists(hash_key, 'data')
+        expect(res).to_be_true()
+
+        job_id = t.jobs[0].job_id
+        keys = app.redis.keys()
+        next_job_id = [
+            key for key in keys if key.decode('utf-8').startswith('rq:job')
+            and not key.decode('utf-8').endswith(job_id)
+        ]
+        expect(next_job_id).to_length(1)
+        next_job_id = next_job_id[0]
+
+        res = app.redis.exists(next_job_id)
+        expect(res).to_be_true()
+
+        res = app.redis.hget(next_job_id, 'status')
+        expect(res).to_equal('queued')
+
+        res = app.redis.hexists(next_job_id, 'data')
+        expect(res).to_be_true()
+
+        res = app.redis.hget(next_job_id, 'origin')
+        expect(res).to_equal('jobs')
+
+        res = app.redis.hget(next_job_id, 'description')
+        expect(res).to_equal(
+            f"easyq.worker.job.run_job('{task_id}', '{job_id}')")
+
+        res = app.redis.hget(next_job_id, 'timeout')
+        expect(res).to_equal('-1')
