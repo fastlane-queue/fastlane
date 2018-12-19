9da1b9d8fa6a9b975fbb577800dcd115527729ad
Bernardo Heynemann <heynemann@gmail.com>
2018-12-19 16:03:04 -0200

    refactored a bit

--- fastlane/api/enqueue.py
+++ fastlane/api/enqueue.py
@@ -24,6 +24,78 @@ def get_details():
     return details
 
 
+def create_job(details, task, logger):
+    logger.debug("Creating job...")
+    retries = details.get("retries", 0)
+    expiration = details.get("expiration")
+
+    hard_limit = current_app.config["HARD_EXECUTION_TIMEOUT_SECONDS"]
+    timeout = details.get("timeout", hard_limit)
+    timeout = min(
+        timeout, hard_limit
+    )  # ensure  jobs can't specify more than hard limit
+
+    j = task.create_job()
+    j.metadata["retries"] = retries
+    j.metadata["retry_count"] = 0
+    j.metadata["expiration"] = expiration
+    j.metadata["timeout"] = timeout
+    j.metadata["envs"] = details.get("envs", {})
+    j.save()
+    logger.debug("Job created successfully...", job_id=str(j.id))
+
+    return j
+
+
+def enqueue_job(task, job, image, command, start_at, start_in, cron, logger):
+    scheduler = Scheduler("jobs", connection=current_app.redis)
+
+    args = [task.task_id, str(job.id), image, command]
+
+    queue_job_id = None
+
+    if start_at is not None:
+        dt = datetime.utcfromtimestamp(int(start_at))
+        logger.debug("Enqueuing job execution in the future...", start_at=dt)
+        result = scheduler.enqueue_at(dt, run_job, *args)
+        job.metadata["enqueued_id"] = str(result.id)
+        queue_job_id = str(result.id)
+        job.save()
+        logger.info("Job execution enqueued successfully.", start_at=dt)
+    elif start_in is not None:
+        dt = datetime.now(tz=timezone.utc) + start_in
+        logger.debug("Enqueuing job execution in the future...", start_at=dt)
+        result = scheduler.enqueue_at(dt, run_job, *args)
+        job.metadata["enqueued_id"] = str(result.id)
+        queue_job_id = str(result.id)
+        job.save()
+        logger.info("Job execution enqueued successfully.", start_at=dt)
+    elif cron is not None:
+        logger.debug("Enqueuing job execution using cron...", cron=cron)
+        result = scheduler.cron(
+            cron,  # A cron string (e.g. "0 0 * * 0")
+            func=run_job,
+            args=args,
+            repeat=None,
+            queue_name="jobs",
+        )
+        job.metadata["enqueued_id"] = str(result.id)
+        queue_job_id = str(result.id)
+        job.metadata["cron"] = cron
+        job.scheduled = True
+        job.save()
+        logger.info("Job execution enqueued successfully.", cron=cron)
+    else:
+        logger.debug("Enqueuing job execution...")
+        result = current_app.job_queue.enqueue(run_job, *args, timeout=-1)
+        queue_job_id = result.id
+        job.metadata["enqueued_id"] = result.id
+        job.save()
+        logger.info("Job execution enqueued successfully.")
+
+    return queue_job_id
+
+
 @bp.route("/tasks/<task_id>", methods=("POST",))
 def create_task(task_id):
     details = get_details()
@@ -46,25 +118,8 @@ def create_task(task_id):
     task = Task.objects(task_id=task_id).modify(task_id=task_id, upsert=True, new=True)
     logger.info("Task created successfully.")
 
-    logger.debug("Creating job...")
-    retries = details.get("retries", 0)
-    expiration = details.get("expiration")
-
-    hard_limit = current_app.config["HARD_EXECUTION_TIMEOUT_SECONDS"]
-    timeout = details.get("timeout", hard_limit)
-    timeout = min(
-        timeout, hard_limit
-    )  # ensure  jobs can't specify more than hard limit
-
-    j = task.create_job()
-    j.metadata["retries"] = retries
-    j.metadata["retry_count"] = 0
-    j.metadata["expiration"] = expiration
-    j.metadata["timeout"] = timeout
-    j.metadata["envs"] = details.get("envs", {})
-    j.save()
+    j = create_job(details, task, logger)
     job_id = str(j.id)
-    logger.debug("Job created successfully...", job_id=job_id)
 
     queue_job_id = None
 
@@ -77,45 +132,9 @@ def create_task(task_id):
             "Only ONE of 'startAt', 'startIn' and 'cron' should be in the request.", 400
         )
 
-    scheduler = Scheduler("jobs", connection=current_app.redis)
-
-    args = [task_id, job_id, image, command]
-
-    if start_at is not None:
-        dt = datetime.utcfromtimestamp(int(start_at))
-        logger.debug("Enqueuing job execution in the future...", start_at=dt)
-        result = scheduler.enqueue_at(dt, run_job, *args)
-        j.metadata["enqueued_id"] = result.id
-        j.save()
-        logger.info("Job execution enqueued successfully.", start_at=dt)
-    elif start_in is not None:
-        dt = datetime.now(tz=timezone.utc) + start_in
-        logger.debug("Enqueuing job execution in the future...", start_at=dt)
-        result = scheduler.enqueue_at(dt, run_job, *args)
-        j.metadata["enqueued_id"] = result.id
-        j.save()
-        logger.info("Job execution enqueued successfully.", start_at=dt)
-    elif cron is not None:
-        logger.debug("Enqueuing job execution using cron...", cron=cron)
-        result = scheduler.cron(
-            cron,  # A cron string (e.g. "0 0 * * 0")
-            func=run_job,
-            args=args,
-            repeat=None,
-            queue_name="jobs",
-        )
-        j.metadata["enqueued_id"] = result.id
-        j.metadata["cron"] = cron
-        j.scheduled = True
-        j.save()
-        logger.info("Job execution enqueued successfully.", cron=cron)
-    else:
-        logger.debug("Enqueuing job execution...")
-        result = current_app.job_queue.enqueue(run_job, *args, timeout=-1)
-        queue_job_id = result.id
-        j.metadata["enqueued_id"] = result.id
-        j.save()
-        logger.info("Job execution enqueued successfully.")
+    queue_job_id = enqueue_job(
+        task, j, image, command, start_at, start_in, cron, logger
+    )
 
     job_url = url_for("task.get_job", task_id=task_id, job_id=job_id, _external=True)
     task_url = url_for("task.get_task", task_id=task_id, _external=True)
