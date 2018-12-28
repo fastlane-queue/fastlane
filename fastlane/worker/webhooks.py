2999c8e5f8444c7268110f78199697630e9e19b4
Bernardo Heynemann <heynemann@gmail.com>
2018-12-28 11:42:56 -0200

    Additional webhooks tests

--- fastlane/worker/webhooks.py
+++ fastlane/worker/webhooks.py
@@ -2,24 +2,58 @@
 from requests import Request, Session
 
 
+class WebhooksDispatchError(RuntimeError):
+    def __init__(self, status_code, method, url, body, headers, error=None):
+        self.status_code = status_code
+        self.method = method
+        self.url = url
+        self.body = body
+        self.headers = headers
+        self.error = error
+
+    def __str__(self):
+        if self.error is None:
+            return (
+                f"The {self.method.upper()} request to {self.url} with body"
+                f' of "{self.body[:10]}..." failed with status code of {self.status_code}.'
+            )
+
+        error = "{}: {}".format(type(self.error).__name__, self.error)
+
+        return (
+            f"The {self.method.upper()} request to {self.url} with body "
+            f'of "{self.body[:10]}..." failed with exception of {error}.'
+        )
+
+    def __repr__(self):
+        return str(self)
+
+
 class Response:
-    def __init__(self, status_code, body, headers=None):
+    def __init__(self, status_code, body, headers):
         self.status_code = status_code
         self.body = body
         self.headers = headers
 
-        if self.headers is None:
-            self.headers = {}
-
 
 class WebhooksDispatcher:
     def dispatch(self, method, url, body, headers, timeout=1):
-        s = Session()
+        try:
+            s = Session()
 
-        req = Request(method, url, data=body, headers=headers)
-        prepped = s.prepare_request(req)
-        prepped.body = body
+            req = Request(method, url, data=body, headers=headers)
+            prepped = s.prepare_request(req)
+            prepped.body = body
 
-        resp = s.send(prepped, timeout=timeout, verify=False)
+            resp = s.send(prepped, timeout=timeout, verify=False)
 
-        return Response(resp.status_code, resp.text, resp.headers)
+            if resp.status_code > 399:
+                raise WebhooksDispatchError(
+                    resp.status_code, method, url, body, headers
+                )
+
+            return Response(resp.status_code, resp.text, resp.headers)
+        except Exception as err:
+            if isinstance(err, WebhooksDispatchError):
+                raise err
+            raise WebhooksDispatchError(500, method, url, body, headers, error=err)
--- tests/worker/test_webhooks.py
+++ tests/worker/test_webhooks.py
@@ -1,4 +1,5 @@
 # 3rd Party
+import requests
 import requests_mock
 from preggy import expect
 
@@ -22,3 +23,39 @@ def test_webhooks(**kwargs):
     expect(r.status_code).to_equal(status_code)
     expect(r.body).to_equal(text)
     expect(r.headers).to_be_like(headers)
+
+
+@requests_mock.Mocker(kw="rm")
+def test_webhooks_2(**kwargs):
+    """Test webhooks fail on 500"""
+    rm = kwargs["rm"]
+    url = "http://some.test.url"
+    text = "whatever"
+    headers = {"qwe": "rty"}
+    rm.get(url, text=text, status_code=500, headers=headers)
+
+    h = webhooks.WebhooksDispatcher()
+    msg = 'The GET request to http://some.test.url with body of "whatever..." failed with status code of %d.'
+    with expect.error_to_happen(webhooks.WebhooksDispatchError, message=msg % 500):
+        h.dispatch("get", url, text, {})
+
+    rm.get(url, text=text, status_code=400, headers=headers)
+    with expect.error_to_happen(webhooks.WebhooksDispatchError, message=msg % 400):
+        h.dispatch("get", url, text, {})
+
+
+@requests_mock.Mocker(kw="rm")
+def test_webhooks_3(**kwargs):
+    """Test webhooks fail on error"""
+    rm = kwargs["rm"]
+    url = "http://some.test.url"
+    text = "whatever"
+    rm.get(url, exc=requests.exceptions.ConnectTimeout("something"))
+
+    h = webhooks.WebhooksDispatcher()
+    msg = (
+        'The GET request to http://some.test.url with body of "whatever..." '
+        "failed with exception of ConnectTimeout: something."
+    )
+    with expect.error_to_happen(webhooks.WebhooksDispatchError, message=msg):
+        h.dispatch("get", url, text, {})
