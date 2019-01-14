dd17c5e99f84ce78b432c85307f4c0fdb1f97330
Bernardo Heynemann <heynemann@gmail.com>
2019-01-14 18:47:14 -0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 # html_theme = "alabaster"
-html_theme = "nature"
+# html_theme = "nature"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
