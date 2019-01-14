bb76696ba86d98a6145f5061b0a63d572dd6395a
Bernardo Heynemann <heynemann@gmail.com>
2019-01-14 18:48:21 -0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -90,6 +90,10 @@ pygments_style = None
 #
 # html_theme_options = {}
 
+html_context = {
+    "css_files": ["_static/css/custom.css"]  # override wide tables in RTD theme
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
