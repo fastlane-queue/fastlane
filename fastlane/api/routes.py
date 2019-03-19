02f9fbd87ed665934b6528ae9520dd8c2ca64712
Janilton Maciel <janilton@gmail.com>
2019-03-19 11:56:44 -0300

    feat: print routes api

---
+++ fastlane/api/routes.py
@@ -0,0 +1,29 @@
+
+
+from flask import Blueprint, jsonify, current_app, abort
+
+bp = Blueprint('routes', __name__) # pylint: disable=invalid-name
+
+
+@bp.route('/routes', methods=['GET'])
+def routes():  # pragma: no cover
+    """Print available functions."""
+    if not current_app.debug:
+        abort(404)
+
+    func_list = []
+    for rule in current_app.url_map.iter_rules():
+        endpoint = rule.rule
+        methods = ", ".join(list(rule.methods))
+        doc = current_app.view_functions[rule.endpoint].__doc__
+
+        route = {
+            "endpoint": endpoint,
+            "methods": methods
+        }
+        if doc:
+            route["doc"] = doc
+        func_list.append(route)
+
+    func_list = sorted(func_list, key=lambda k: k['endpoint'])
+    return jsonify(func_list)
