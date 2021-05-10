0061cfdd512e7064ba716e97b402637330aa12ea
João Meyer <1994meyer@gmail.com>
2021-05-10 16:32:06 -0300

    Now using patch in tasks tests

--- tests/test_api_tasks.py
+++ tests/test_api_tasks.py
@@ -11,54 +11,62 @@ from newlane import crud
 
 class TestApiTasks(TestCase):
     def setUp(self):
-        crud.task = mock.Mock()
-        crud.task.page = mock.AsyncMock()
-        crud.task.get_or_404 = mock.AsyncMock()
-        crud.task.get_or_create = mock.AsyncMock()
+        self.crud_patch = mock.patch('newlane.api.tasks.crud')
+
+        self.crud = self.crud_patch.start()
+
+        self.crud.task = mock.Mock()
+        self.crud.task.page = mock.AsyncMock()
+        self.crud.task.get_or_404 = mock.AsyncMock()
+        self.crud.task.get_or_create = mock.AsyncMock()
+
         self.app = TestClient(app.app)
+
+    def tearDown(self):
+        self.crud_patch.stop()
 
     def test_post_task(self):
         """ Posts task """
-        crud.task.get_or_create.return_value = {}
+        self.crud.task.get_or_create.return_value = {}
 
         body = {'name': 'nice'}
         response = self.app.post('/tasks/', json=body)
 
         self.assertEqual(response.status_code, 200)
 
-        crud.task.get_or_create.assert_called_once_with(name='nice')
-        crud.task.get_or_create.assert_awaited()
+        self.crud.task.get_or_create.assert_called_once_with(name='nice')
+        self.crud.task.get_or_create.assert_awaited()
 
     def test_get_task(self):
         """ Gets task """
-        crud.task.get_or_404.return_value = {}
+        self.crud.task.get_or_404.return_value = {}
 
         response = self.app.get('/tasks/nice')
 
         self.assertEqual(response.status_code, 200)
 
-        crud.task.get_or_404.assert_called_once_with(name='nice')
-        crud.task.get_or_404.assert_awaited()
+        self.crud.task.get_or_404.assert_called_once_with(name='nice')
+        self.crud.task.get_or_404.assert_awaited()
 
     def test_get_task_404(self):
         """ Gets task, 404 """
-        crud.task.get_or_404.side_effect = HTTPException(404)
+        self.crud.task.get_or_404.side_effect = HTTPException(404)
 
         response = self.app.get('/tasks/nice')
 
         self.assertEqual(response.status_code, 404)
 
-        crud.task.get_or_404.assert_called_once_with(name='nice')
-        crud.task.get_or_404.assert_awaited()
+        self.crud.task.get_or_404.assert_called_once_with(name='nice')
+        self.crud.task.get_or_404.assert_awaited()
 
     def test_get_tasks(self):
         """ Gets tasks """
-        crud.task.page.return_value = {}
+        self.crud.task.page.return_value = {}
 
         params = {'page': 7, 'size': 4}
         response = self.app.get('/tasks', params=params)
 
         self.assertEqual(response.status_code, 200)
 
-        crud.task.page.assert_called_once_with(page=7, size=4)
-        crud.task.page.assert_awaited()
+        self.crud.task.page.assert_called_once_with(page=7, size=4)
+        self.crud.task.page.assert_awaited()
